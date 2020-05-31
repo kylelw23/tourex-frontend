2677547ed74a926099d51d2619e5d5f56624b62a
febb <fl988@uowmail.edu.au>
2020-06-01 01:45:27 +1000

    landing page now showing again.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './Ultil.css';
-import {Route, Switch} from 'react-router-dom';
-import LandingPage from './Pages/LandingPage';
-import { fetchTourexAPIToken } from "./Components/auth/Auth";
-import Navbar from './Components/NavBar';
+import React from "react";
+import logo from "./logo.svg";
+import "./Ultil.css";
+import { Route, Switch } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
+// import { fetchTourexAPIToken } from "./Components/auth/Auth";
+import Navbar from "./Components/NavBar";
+import Banner from "./Components/Banner";
 
 function App() {
   const [getToken, setToken] = React.useState("");
 
   // delete this later, just for testings.
-  async function grabToken() {
-    let token = await fetchTourexAPIToken();
-    setToken("token is " + token);
-  }
+  // async function grabToken() {
+  //   let token = await fetchTourexAPIToken();
+  //   setToken("token is " + token);
+  // }
 
   return (
     <React.Fragment>
       <Switch>
-        <Navbar>
-        <Route exact path="/" component={LandingPage}/>
-        </Navbar>
+        <>
+          {/* This <> fragment fixed the 'hello world' not showing. */}
+          <Navbar />
+          <Route exact path="/" component={LandingPage} />
+        </>
       </Switch>
     </React.Fragment>
   );
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../Assets/Images/italy_stretched.png";
 
 export default class LandingPage extends React.Component {
-	render () {
-		return (
-			<div>
-				Hello world
-			</div>
-		)
-	}
+  render() {
+    return (
+      // maybe this banner can be another component??
+      <div class="jumbotron" style={{ backgroundImage: "url(" + logo + ")" }}>
+        <div className="row">
+          <div className="col-lg-6">
+            <div
+              className="card"
+              style={{ backgroundColor: "#f5f5f5", opacity: 0.9 }}
+            >
+              <div className="card-body">
+                <h1 class="display-4">
+                  Great things never came from comfort zones!
+                </h1>
+                <hr class="my-4" />
+                <p>
+                  Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+                  used in laying out print, graphic or web designs.
+                </p>
+                <form class="form-inline my-2 my-lg-0">
+                  <input
+                    class="form-control mr-sm-2"
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button
+                    class="btn btn-outline-info my-2 my-sm-0"
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
