a1777451f7e81575e3b3b36eaa715d5c614e485f
kylelw23 <dkb139@uowmail.edu.au>
2020-06-01 00:40:00 +1000

    Bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Ultil.css';
 import {Route, Switch} from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import { fetchTourexAPIToken } from "./Components/auth/Auth";
+import Navbar from './Components/NavBar';
 
 function App() {
   const [getToken, setToken] = React.useState("");
@@ -17,7 +18,9 @@ function App() {
   return (
     <React.Fragment>
       <Switch>
+        <Navbar>
         <Route exact path="/" component={LandingPage}/>
+        </Navbar>
       </Switch>
     </React.Fragment>
   );
